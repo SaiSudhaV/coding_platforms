ed44fa85779816a2f865b2d60230828560cb33c8
sai_sudha <saisudhavadisina@gmail.com>
2021-04-12 23:54:41 +0530

    Find the minimum appends to be done for making it as a palindrome

---
+++ min_palindrome.py
@@ -0,0 +1,18 @@
+class Solution:
+    # @param A : string
+    # @return an integer
+    def is_palindrome(self, A, m, n):
+        while m < n:
+            if A[m] != A[n]:
+                return False
+        return True
+    
+    def solve(self, A):
+        n = len(A)
+        low, high, res = 0, n - 1, 0
+        while low < high:
+            if A[low] == A[high]:
+                if self.is_palindrome(A, low, high):
+                    return res
+            res, low = res + 1, low + 1
+        return res
