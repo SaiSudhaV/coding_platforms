5754ad823fa3ce708a5ec97cce36cb2a8b83f295
sai_sudha <saisudhavadisina@gmail.com>
2021-04-17 23:21:58 +0530

    Corrected to get the required output

--- count_possible_ids.py
+++ count_possible_ids.py
@@ -1,18 +1,21 @@
-def power(x, n):
+#your code goes here 
+
+def calculate(x, n):
     if n == 0:
         return 1
     if n % 2 == 1:
-        return power(x, n - 1) * x % 1000000007
-    res = power(x, n >> 1)
+        return calculate(x, n - 1) * x % 1000000007
+    res = calculate(x, n >> 1)
     return res ** 2
 
 def possible_id_count(n, k):
-    num = power(n, k + 1) - 1 + 1000000007
-    den = power(n - 1, 1000000007 - 2)
-    print(num * den % 1000000007 - 1)
+    num = calculate(n, k + 1) - 1 + 1000000007
+    den = calculate(n - 1, 1000000007 - 2)
+    return num * den % 1000000007 - 1
 
 if __name__ == "__main__":
     while True:
         n, k = map(int, input().split())
         if n == 0 and k == 0:
             break
+        print(possible_id_count(n, k))
