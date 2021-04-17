f2e925ab2dacbea7b6d473bba953f6d84f48e914
sai_sudha <saisudhavadisina@gmail.com>
2021-04-17 22:49:29 +0530

    Checks whether the numbers are possible to become squares of two or not

---
+++ two_squares.py
@@ -0,0 +1,18 @@
+def squares(n):
+    i = 2
+    while (i ** 2 <= n):
+        count = 0
+        if n % i == 0:
+            while n % i == 0:
+                count += 1
+                n /= i
+            if n % 4 == 4 and count % 2 != 0):
+                return "No"
+        i += 1
+    return "Yes" if c % 4 !=3 else "No"
+
+if __name__ == "__main__":
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        print(valid_two_squares(n))
