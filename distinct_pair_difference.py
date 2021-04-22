67cb703082c4cda960746db6d30409834f628deb
sai_sudha <saisudhavadisina@gmail.com>
2021-04-22 23:58:01 +0530

    Finds the minimum distinct pair difference

---
+++ distinct_pair_difference.py
@@ -0,0 +1,15 @@
+#your code goes here
+
+def distinct_pair_difference(ar, n):
+    tem, res = [0 for i in range(n)], []
+    for i in range(1, n):
+        tem[i] = tem[i - 1] + ar[i]
+    for i in range(n):
+        res.append((i + 1) * ar[i] - tem[i])
+    return sum(res)
+
+if __name__ == "__main__":
+    t = int(input())
+    for i in range(t):
+        n, ar = int(input()), list(map(int, input().split()))
+        print(distinct_pair_difference(ar, n))
