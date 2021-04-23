a36b47c8d43145c88c34c6c6d6d341598bc46fa0
sai_sudha <saisudhavadisina@gmail.com>
2021-04-23 17:33:13 +0530

    Finds the distinct pair difference

--- distinct_pair_difference.py
+++ distinct_pair_difference.py
@@ -1,13 +1,12 @@
-#your code goes here
-
 def distinct_pair_difference(ar, n):
     tem, res = [0 for i in range(n)], []
+    tem[0] = ar[0]
     for i in range(1, n):
         tem[i] = tem[i - 1] + ar[i]
     for i in range(n):
         res.append((i + 1) * ar[i] - tem[i])
     return sum(res)
-
+ 
 if __name__ == "__main__":
     t = int(input())
     for i in range(t):
