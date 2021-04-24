0c0d15fd4370939e930da0c167ea98c6d9592949
sai_sudha <saisudhavadisina@gmail.com>
2021-04-24 22:28:16 +0530

    finding nearest rounded square root of a given number

---
+++ fsqrt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<math.h>
+int main(void) {
+	// your code goes here
+	int t,i,n,y;
+	scanf("%d",&t);
+	for(i=0;i<t;i++)
+	{
+	    scanf("%d",&n);
+	    y=sqrt(n);
+	    printf("%d\n",y);
+
+	}
+	return 0;
+}
