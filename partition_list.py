04cfafa88672a13581ea36f0fd2f96695f93e6b8
sai_sudha <saisudhavadisina@gmail.com>
2021-05-04 22:04:44 +0530

    Given list is partitioned and returns the list which has elements less than x come before the node

---
+++ partition_list.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#    def __init__(self, x):
+#        self.val = x
+#        self.next = None
+
+class Solution:
+    # @param A : head node of linked list
+    # @param B : integer
+    # @return the head node in the linked list
+    def partition(self, A, B):
+        if A.next == None:
+            return A
+        else:
+            res = ListNode(-1)
+            res.next, temp, ptr = A, res, A
+            while ptr.val < B:
+                ptr, temp = ptr.next, temp.next
+            p = temp
+            while ptr:
+                if ptr.val < B:
+                    temp.next, ptr.next, p.next = ptr.next, p.next, ptr
+                    p, ptr = ptr, temp.next
+                else:
+                    ptr, temp = ptr.next, temp.next
+        res = res.next
+        return res
