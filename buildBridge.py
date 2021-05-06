984299f25a7f4231ae3487c3598aaac799c28b39
sai_sudha <saisudhavadisina@gmail.com>
2021-05-06 23:23:09 +0530

    Finds the number ways of building bridge

---
+++ buildBridge.py
@@ -0,0 +1,15 @@
+a = int(input())
+b = int(input())
+c = int(input())
+add = a * 1 + b * 5
+add1 = a * 5 + b +1
+s = (a * b) * 1
+s1 = (a * b) * 5
+s2 = a * 5
+s3 = a * 1
+s4 = b * 1
+s5 = b * 5
+if add == c or add1 == c or s1 == c or s == c or s2 == c or s3 == c or s4 == c or s5 == c:
+    print(True)
+else:
+    print(False)
