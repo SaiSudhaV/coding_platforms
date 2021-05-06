443c20cd6154d64c3072706e8722a6460067955a
sai_sudha <saisudhavadisina@gmail.com>
2021-05-06 23:20:28 +0530

    Finding number of candles that can be blown

---
+++ Candles.py
@@ -0,0 +1,16 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthdayCakeCandles function below.
+def birthdayCakeCandles(ar):
+    largest = max(ar)
+    return ar.count(largest)
+
+if __name__ == '__main__':
+    ar_count = int(input())
+    ar = list(map(int, input().rstrip().split()))
+    result = birthdayCakeCandles(ar)
+    print(str(result))
