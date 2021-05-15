33cdeeb5f11112c02e75b29ee723bc81d970fc33
sai_sudha <saisudhavadisina@gmail.com>
2021-05-16 00:07:44 +0530

    Finds the least possible subsequence

--- common_subsequence.py
+++ common_subsequence.py
@@ -4,7 +4,7 @@ def possible_smallest_subsequence(ar, br, n, m):
     res = 1
     for i in ar:
         if i in br:
-            return "".join(str(i) for i in ['YES\n1', i])
+            return " ".join(str(i) for i in ['YES\n1', i])
     return "NO"
 	
 if __name__ == "__main__":
