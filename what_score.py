0f8eee6754b6754a93d080b497e98cb23861930c
sai_sudha <saisudhavadisina@gmail.com>
2021-05-16 23:09:40 +0530

    Calculates the score

---
+++ what_score.py
@@ -0,0 +1,22 @@
+# cook your dish here
+
+def calculate_score(ar, br, n):
+    for i in range(n):
+        for j in range(i + 1, n):
+            if ar[i] == ar[j]:
+                if br[i] <= br[j]:
+                    br[i] = 0
+                else:
+                    br[j] = 0
+    return sum(br)
+
+if __name__ == "__main__":
+    t = int(input())
+    for i in range(t):
+        n =  int(input())
+        ar, br = [], []
+        for i in  range(n):
+            p, s = map(int, input().split())
+            ar.append(p)
+            br.append(s)
+        print(calculate_score(ar, br, len(ar)))
