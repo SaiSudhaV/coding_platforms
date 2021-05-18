435ce996acdc5770921bce2c57f0867644cc4757
sai_sudha <saisudhavadisina@gmail.com>
2021-05-18 23:06:27 +0530

    Reversing the array elements

---
+++ reverseArray.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+def reverse(ar):
+    lst = [i for i in ar]
+    lst1 = reversed(lst)
+    return ' '.join([str(elem) for elem in lst1])
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    ar_count = int(input())
+    
+    ar = list(map(int, input().rstrip().split()))
+    
+    result = reverse(ar)
+    
+    fptr.write(result + '\n')
+    
+    fptr.close()
