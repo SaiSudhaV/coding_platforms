ff5aba0e8faa251fe2f645a227a4a1d78b766647
sai_sudha <saisudhavadisina@gmail.com>
2021-05-20 23:35:16 +0530

    Checks the possibilty of a worker to work on weekends

--- lost_weekends.py
+++ lost_weekends.py
@@ -1,10 +1,10 @@
 # cook your dish here
 
-def check_possibility(ar, p):
-    return "Yes" if sum([i * p for i in ar]) == 120 else "No"
+def check_possibility(a, b, c, d, e):
+    return "Yes" if sum([a, b, c, d, e]) > 120 else "No"
 
 if __name__ == "__main__":
     t = int(input())
     for i in range(t):
-        ar = list(map(int, input().split()))
-        print(check_possibility(ar[: 4], ar[-1]))
+        a, b, c, d, e, k = map(int, input().split())
+        print(check_possibility(a * k, b * k, c * k, d * k, e * k))
