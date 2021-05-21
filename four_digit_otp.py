452c49d15daa9814a3e79811457494379d521b45
sai_sudha <saisudhavadisina@gmail.com>
2021-05-21 21:22:41 +0530

    Generates a four digit otp from the given string

---
+++ four_digit_otp.py
@@ -0,0 +1,10 @@
+# You will be given a number in the form of string, extract out digits at odd places, square & merge them. First 4 digits will be the required OTP.
+
+def gen_OTP(ar, n):
+    tem = [int(ar[i]) for i in range(n) if i % 2 != 0]
+    res = "".join(str(i * i) for i in tem)
+    return res[:4]
+
+if __name__ == "__main__":
+    s = input()
+    print(gen_OTP(list(s), len(s)))
