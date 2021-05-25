e58091b6f4486f27f0c7fd71509aa36adc88e817
sai_sudha <saisudhavadisina@gmail.com>
2021-05-25 22:36:18 +0530

    Returns the deep copy of a list

--- copy_list.py
+++ copy_list.py
@@ -1,11 +1,10 @@
 from collections import defaultdict as dedt
     
-# Definition for singly-linked list with a random pointer.
-# class RandomListNode:
-#     def __init__(self, x):
-#         self.label = x
-#         self.next = None
-#         self.random = None
+class RandomListNode:
+    def __init__(self, x):
+        self.label = x
+        self.next = None
+        self.random = None
 
 class Solution:
     # @param head, a RandomListNode
