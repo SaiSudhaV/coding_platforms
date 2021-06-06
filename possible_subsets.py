01724df8ea6c41fb90f6e265948ecdd288f74bfa
sai_sudha <saisudhavadisina@gmail.com>
2021-06-07 00:14:56 +0530

    Formatted output according to the required format

--- possible_subsets.py
+++ possible_subsets.py
@@ -13,8 +13,9 @@ def solve(A, tem, p, res):
 def subsets(A):
     res = []
     solve(sorted(A), [], 0, res)
-    res = [" ". join(str(i) for j in res for i in j)]
-    return "\n".join(i for i in res)
+    res = [i for i in res]
+    res.remove([])
+    return "\n".join(" ".join(str(j) for j in i) for i in res)
 
 if __name__ == "__main__":
     t = int(input())
@@ -22,3 +23,4 @@ if __name__ == "__main__":
         n = int(input())
         ar = list(map(int, input().split()))
         print(subsets(ar))
+        print()
