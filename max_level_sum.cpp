d27dec73a2c9e9b17652582740aca5850c8b137b
sai_sudha <saisudhavadisina@gmail.com>
2021-07-03 23:44:14 +0530

    Finds the sum of the elements in maximum level

--- max_level_sum.cpp
+++ max_level_sum.cpp
@@ -1,22 +1,21 @@
 int Solution::solve(TreeNode* A) {
     queue<TreeNode*> tem1, tem2;
-    int m = 0, temp=0;
+    TreeNode* node;
+    int res = 0, temp=0;
     tem1.push(A);
     while(!tem1.empty()) {
-        TreeNode* node = tem1.front();
+        node = tem1.front();
         tem1.pop();
         temp += node -> val;
-        
         if(node -> left)
             tem2.push(node->left);
         if(node->right)
             tem2.push(node->right);
-        
         if(tem1.empty()) {
-            tem1.swap(t);
-            m = max(m, temp);
+            res = max(res, temp);
             temp = 0;
+            tem1.swap(tem2);
         }
     }
-    return max(m, temp);
+    return max(res, temp);
 }
